b3c3355948a7dc79757e3664dbc20c0b5148987f
adzample <adzample@gmail.com>
2015-10-27 20:01:21 +1100

    many changes

--- server/api/contact/contact.controller.js
+++ server/api/contact/contact.controller.js
@@ -3,78 +3,31 @@
 var _ = require('lodash');
 var Contact = require('./contact.model'); 
 var nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport();
-
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'hayden94103@gmail.com',    // your email here
+    pass: 'tropical3'         // your password here
+  }
+});
+// Get list of mailers
 exports.sendMail = function(req, res) {
- 
-    var data = req.body;
- 
-    transporter.sendMail({
-        from: data.email,
-        to: 'dankwheels@gmail.com',
-        subject: 'Message from ' + data.name,
-        text: data.message
-    }, function (error, info) {
-      if (error) {
-        res.sendStatus(500);
-      } else {
-        res.sendStatus(200);
-      }
-    })
-    
- 
-};
-// Get list of contacts
-exports.index = function(req, res) {
-  Contact.find(function (err, contacts) {
-    if(err) { return handleError(res, err); }
-    return res.status(200).json(contacts);
+  var htmlContent = '<p>Name: ' + req.body.name + '</p>' +
+                    '<p>Email: ' + req.body.email + '</p>' +
+                    '<p>Message: ' + req.body.message + '</p>';
+  var mailOptions = {
+    to: 'robbieferguson139@gmail.com',                  // your email here
+    subject: 'New message from Dwheels',
+    from: req.body.name + ' <' + req.body.email + '>',
+    sender: req.body.email,
+    html: htmlContent
+  };
+  transporter.sendMail(mailOptions, function(err, info){
+    if (err) {
+      console.log(err);
+    }else{
+      console.log('Message sent: ' + info.response);
+      return res.end('{"success" : "Updated Successfully", "status" : 200}');
+    }
   });
 };
-
-// Get a single contact
-exports.show = function(req, res) {
-  Contact.findById(req.params.id, function (err, contact) {
-    if(err) { return handleError(res, err); }
-    if(!contact) { return res.status(404).send('Not Found'); }
-    return res.json(contact);
-  });
-};
-
-// Creates a new contact in the DB.
-exports.create = function(req, res) {
-  Contact.create(req.body, function(err, contact) {
-    if(err) { return handleError(res, err); }
-    return res.status(201).json(contact);
-  });
-};
-
-// Updates an existing contact in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  Contact.findById(req.params.id, function (err, contact) {
-    if (err) { return handleError(res, err); }
-    if(!contact) { return res.status(404).send('Not Found'); }
-    var updated = _.merge(contact, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.status(200).json(contact);
-    });
-  });
-};
-
-// Deletes a contact from the DB.
-exports.destroy = function(req, res) {
-  Contact.findById(req.params.id, function (err, contact) {
-    if(err) { return handleError(res, err); }
-    if(!contact) { return res.status(404).send('Not Found'); }
-    contact.remove(function(err) {
-      if(err) { return handleError(res, err); }
-      return res.status(204).send('No Content');
-    });
-  });
-};
-
-function handleError(res, err) {
-  return res.status(500).send(err);
-}
--- server/api/contact/index.js
+++ server/api/contact/index.js
@@ -4,12 +4,6 @@ var express = require('express');
 var controller = require('./contact.controller');
 
 var router = express.Router();
-
-router.get('/', controller.index);
-router.get('/:id', controller.show);
 router.post('/', controller.sendMail);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
 
 module.exports = router;
